48c1858ebdce4d315a4ad5f07fc6e4336f666fb3
Chris Baker <baker@svnetwork.com>
2011-04-08 13:32:35 -0700

    bug is reproducible at this point

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110408190520) do
 
   create_table "posts", :id => false, :force => true do |t|
     t.integer  "id",         :null => false
