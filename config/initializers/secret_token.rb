e10bfe4a9a15aa68df0030b26d8e93324055563c
Chris Baker <baker@svnetwork.com>
2011-04-08 11:46:11 -0700

    Created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OctopusBug::Application.config.secret_token = '67dabbe8d47dc272b1f00ee52da409d5713abc8d4bc9f8b04a506251250d55729e4ab539f4cd2da549b8ba892694f74c628195517f21ba46d17f950e6387920c'
